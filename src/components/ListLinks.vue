5351f7f8e3fd3590a3d4c4f2b0edb608db08505b
YamKH <r2726992334@gmail.com>
2025-01-16 00:01:38 +0800

    Add: Links Img

--- src/components/ListLinks.vue
+++ src/components/ListLinks.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import { ref } from 'vue'
+
 defineProps<{
   list: {
     text: string
@@ -7,6 +9,8 @@ defineProps<{
     href: string
   }[]
 }>()
+
+const defaultImage = ref('/default.png')
 </script>
 
 <template>
@@ -19,7 +23,7 @@ defineProps<{
     <li v-for="link in list" :key="link.text" container-link w-full flex items-center rd-2>
       <a flex items-center target="_blank" :href="link.href" :aria-label="link.text">
         <div ml-2 mr-4 pt-2>
-          <i text-4xl inline-block :class="link.icon || 'i-carbon-unknown'" />
+          <img :src="link.icon || defaultImage" alt="Icon" style="width: 4rem; display: inline-block;">
         </div>
         <div font-normal lh-tight>
           <div text-lg hover:text-main>{{ link.text }}</div>
--- src/pages/links/data.ts
+++ src/pages/links/data.ts
@@ -5,15 +5,15 @@ export const linksData: LinksData = [
     title: 'Blog',
     links: [
       {
-        text: '星空之窗',
-        description: '星河景明的个人博客。',
-        icon: 'i-carbon-campsite',
+        text: '星河景明',
+        description: '学如逆水行舟，不进则退。',
+        icon: 'https://minio.drivefly.cn/image-risinggalaxy/myselfWhite.png',
         href: 'https://blog.rising-galaxy.top/',
       },
       {
         text: 'Drive_FLY\'s Blog',
         description: 'Drive_FLY\'s Blog.',
-        icon: 'i-carbon-campsite',
+        icon: '',
         href: 'https://blog.drivefly.cn/',
       },
     ],
@@ -24,7 +24,7 @@ export const linksData: LinksData = [
       {
         text: 'CFC Co-Learning',
         description: 'CFC 共学计划主页。',
-        icon: 'i-carbon-campsite',
+        icon: 'https://learning.bibibai.top/output.png',
         href: 'https://learning.bibibai.top/',
       },
     ],
