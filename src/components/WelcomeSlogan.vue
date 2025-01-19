d16f571541a9c7f8f3afc63444e754c92d3ada1b
YamKH <r2726992334@gmail.com>
2025-01-19 20:13:20 +0800

    Add: WelcomeSlogan

---
+++ src/components/WelcomeSlogan.vue
@@ -0,0 +1,33 @@
+<script  setup lang="ts">
+import { reactive } from 'vue'
+
+const slogan: Array<string> = reactive(['你好👋', '夜深了😴', '清晨了，您早啊🥱', '上午好👋', '事已至此，先吃饭吧🍚', '下午好👋', '晚上好👋'])
+const date = new Date()
+const hour = date.getHours()
+let i = 0
+
+if (hour < 5 || hour >= 23) {
+  i = 1
+}
+else if (hour >= 5 && hour < 7) {
+  i = 2
+}
+else if (hour >= 7 && hour < 11) {
+  i = 3
+}
+else if (hour >= 11 && hour < 13) {
+  i = 4
+}
+else if (hour >= 13 && hour < 18) {
+  i = 5
+}
+else if (hour >= 18 && hour < 23) {
+  i = 6
+}
+</script>
+
+<template>
+  <h1 class="text-title">
+    {{ slogan[i] }}
+  </h1>
+</template>
